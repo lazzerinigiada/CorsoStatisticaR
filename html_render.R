7d98904f0f83b90bff50835677b8d1b4d4cb2864
Giovanni Iacopetti <giovanni.jacopetti@gmail.com>
2018-02-07 11:12:47 +0200

    HTML test2

---
+++ html_render.R
@@ -0,0 +1,3 @@
+rmarkdown::render('README.Rmd', output_format = "html_document", output_file =  "index.html")
+rmarkdown::render('lez01/README.Rmd', output_format = "html_document", output_dir = "lez01", output_file =  "index.html")
+rmarkdown::render('lez02/README.Rmd', output_format = "html_document", output_dir = "lez02", output_file =  "index.html")
