32c91e6782238ee27f8364b51417d642c21c4cda
Giovanni Iacopetti <giovanni.jacopetti@gmail.com>
2018-02-07 11:21:26 +0200

    HTML test finished (badly)

--- html_render.R
+++
@@ -1,3 +0,0 @@
-rmarkdown::render('README.Rmd', output_format = "html_document", output_file =  "index.html")
-rmarkdown::render('lez01/README.Rmd', output_format = "html_document", output_dir = "lez01", output_file =  "index.html")
-rmarkdown::render('lez02/README.Rmd', output_format = "html_document", output_dir = "lez02", output_file =  "index.html")
